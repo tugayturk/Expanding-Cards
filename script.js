8df177f48a381682fc257991cc2bf084c5e5b44b
Tugay Türk <74865603+tugayturk@users.noreply.github.com>
2021-01-05 12:44:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const myPanel = document.querySelectorAll(".panel");
+
+for(let i=0;i<myPanel.length;i++){
+    myPanel[i].addEventListener("click",function () {
+        if(myPanel[i].classList.contains("active")){
+            removePanel()
+        }else{
+            removePanel()
+            myPanel[i].classList.toggle("active")
+        }
+    });
+}
+
+function removePanel() {
+    for(let j = 0;j<myPanel.length;j++){
+        myPanel[j].classList.remove("active")
+    }
+}
